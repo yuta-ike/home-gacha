ebdabc24684872f9e78223e0e0148012e12c9044
yu-3in <misharv.y@gmail.com>
2024-03-03 13:34:59 +0900

    fix: color

--- front/src/components/FormSelectItem.tsx
+++ front/src/components/FormSelectItem.tsx
@@ -11,7 +11,7 @@ interface FormSelectItemProps {
 export const FormSelectItem: React.FC<FormSelectItemProps> = ({value,label, selectedValue, setValue}) => {
   return (
     <Button variant="outline" onClick={() => setValue(value === selectedValue ? "" : value)} className={cn("px-4 py-2 rounded-md font-bold w-full",
-    value === selectedValue && "bg-[#7AC973] focus:bg-[#7AC973] text-white focus:text-white")}>
+    value === selectedValue && "bg-[#7AC973] focus:bg-[#7AC973] focus-visible:bg-[#7AC973] text-white focus:text-white focus-visible:text-white")}>
      {label}
     </Button>
   )
